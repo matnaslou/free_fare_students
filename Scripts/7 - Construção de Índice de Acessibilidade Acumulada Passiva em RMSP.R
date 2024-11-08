2a9bf532e4404c807fcfb69355d0beb716bb564b
matnaslou <matheusnaslou@gmail.com>
2024-11-08 01:23:27 -0300

    aopdata included
    - passive accessibility calculated

--- Scripts/7 - Construção de Índice de Acessibilidade Acumulada Passiva em RMSP.R
+++ Scripts/7 - Construção de Índice de Acessibilidade Acumulada Passiva em RMSP.R
@@ -9,12 +9,24 @@ library(osmdata)
 library(ggplot2)
 library(h3jsr)
 
+#setwd("G:/.shortcut-targets-by-id/13s-t-swy07Av0TJBI4pTBk7Dye0Gk70m/free fare students/")
+
 options(java.parameters = "-Xmx6G")
 
+# Defining the State for analysis
+estado <- "SP"
 
-# Obter a bounding box de São Paulo
-#bbox_sp <- getbb("São Paulo, São Paulo, Brazil")
+# Population Estimation of Sao Paulo based on 2010's Brazilian Census
+aop <- read_population(
+  city = "spo",
+  year = 2010,
+  geometry = FALSE,
+  showProgress = TRUE
+)
+
+# Obtaining Sao Paulo bounding box
 bbox_sp <- getbb("Região Metropolitana de São Paulo, Região Geográfica Intermediária de São Paulo, São Paulo, Região Sudeste, Brasil")
+
 # Criar um polígono com as coordenadas do bounding box
 poligono_sp <- st_as_sfc(st_bbox(c(
   xmin = bbox_sp["x", "min"],
@@ -23,107 +35,115 @@ poligono_sp <- st_as_sfc(st_bbox(c(
   ymax = bbox_sp["y", "max"]
 ), crs = 4326))
 
+# Creating id_hex resolution 9 based on each polygon
 sp <- polygon_to_cells(geometry = poligono_sp, res = 9, simple = TRUE)
 sp <- data.frame(sp)
 colnames(sp)[colnames(sp) == "X0"] <- "id"
 
+# Creating Latitude and Longitude based on id_hex
 sp <- sp %>% 
   mutate(lat = cellToLatLng(id)$lat) %>%
   mutate(lon = cellToLatLng(id)$lng)
 
-sp <- sp %>% 
-  mutate(lat = cellToLatLng(id)$lat) %>%
-  mutate(lon = cellToLatLng(id)$lng)
+#sp <- sp %>% 
+#  mutate(lat = cellToLatLng(id)$lat) %>%
+#  mutate(lon = cellToLatLng(id)$lng)
 
+# NOT NEEDED RIGHT NOW WITH aopdata
+# escolas <- read_schools(year=2020,showProgress = TRUE)
+# escolas <- escolas[(escolas$abbrev_state %in% estado),]
+# 
+# df <- sfheaders::sf_to_df(escolas)[c("x", "y")]
+# df$CODESC <- escolas$code_school
+# df$UF <- escolas$abbrev_state
+# 
+# df <- df[!(df$x %in% NaN),]
+# #df <- df[!(df$y %in% NaN),]
+# 
+# df$resolution <- 9
+# lat = c(df$y)
+# lng = c(df$x)
+# resolution = c(df$resolution,df$resolution)
+# 
+# df <- df[(df$UF %in% estado),]
+# 
+# df <- df %>% 
+#   mutate(id = latLngToCell(lat, lng, resolution))
+# 
+# df <- df %>%
+#   group_by(id) %>%
+#   summarise(
+#     #lat = mean(y, na.rm = TRUE),  # Calcula a média da latitude por id_hex
+#     #lon = mean(x, na.rm = TRUE),  # Calcula a média da longitude por id_hex
+#     contagem_unica = n_distinct(CODESC)   # Conta os valores únicos de CODESC por id_hex
+#   )
+# 
+# df <- df %>% 
+#   mutate(lat = cellToLatLng(id)$lat) %>%
+#   mutate(lon = cellToLatLng(id)$lng)
+# 
+# df_final <- merge(x=sp,y=df, 
+#                by=c("id","lat","lon"), all.x=TRUE)
+# 
+# df_final[is.na(df_final)] <- 0
 
-escolas <- read_schools(year=2020,showProgress = TRUE)
-escolas <- escolas[(escolas$abbrev_state %in% "SP"),]
+# Renaming for merge
+colnames(aop)[colnames(aop) == "id_hex"] <- "id"
 
-df <- sfheaders::sf_to_df(escolas)[c("x", "y")]
-df$CODESC <- escolas$code_school
-df$UF <- escolas$abbrev_state
-
-df <- df[!(df$x %in% NaN),]
-#df <- df[!(df$y %in% NaN),]
-
-df$resolution <- 9
-lat = c(df$y)
-lng = c(df$x)
-resolution = c(df$resolution,df$resolution)
-
-df <- df %>% 
-  mutate(id = latLngToCell(lat, lng, resolution))
-
-df <- df[(df$UF %in% "SP"),]
-
-df <- df %>%
-  group_by(id) %>%
-  summarise(
-    #lat = mean(y, na.rm = TRUE),  # Calcula a média da latitude por id_hex
-    #lon = mean(x, na.rm = TRUE),  # Calcula a média da longitude por id_hex
-    contagem_unica = n_distinct(CODESC)   # Conta os valores únicos de CODESC por id_hex
-  )
-
-df <- df %>% 
-  mutate(lat = cellToLatLng(id)$lat) %>%
-  mutate(lon = cellToLatLng(id)$lng)
-
-df_final <- merge(x=sp,y=df, 
-               by=c("id","lat","lon"), all.x=TRUE)
-
-df_final[is.na(df_final)] <- 0
+# Merging aopdata info with schools
+#mdf <- merge(df_final, aop, by = "id", all = TRUE)
+mdf <- merge(sp, aop, by = "id", all = TRUE)
 
 pasta <- system.file("extdata/spo", package = "r5r")
 pasta
 
-pasta <- "C:/Users/rosan/Downloads/Nova Pasta"
+pasta <- "network_data"
 
 fs::dir_tree(pasta)
 
 conexao_r5r <- setup_r5(pasta, verbose = FALSE)
 fs::dir_tree(pasta)
 
-
-
+# Only execute if necessary: Calculation is very demanding
 matriz <- travel_time_matrix(
   conexao_r5r,
   origins = sp,
   destinations = sp,
   mode = c("WALK", "TRANSIT"),
   departure_datetime = as.POSIXct(
-    "13-05-2019 14:00:00",
+    "13-05-2014 14:00:00",                 # Pre-Treatment travelling
     format = "%d-%m-%Y %H:%M:%S"
   ),
   max_walk_time = 30,
   max_trip_duration = 120,
   verbose = FALSE,
-  progress = FALSE
+  progress = TRUE
 )
 head(matriz)
 
-matriz <- read.csv("C:/Users/rosan/Downloads/matriz.csv", header = TRUE, sep = ",")
+# Exporting Travel Time Matrix calculations
+write.csv(matriz, "matriz.csv", row.names = FALSE)
 
-#data.table::setnames(matriz, "travel_time_p50", "travel_time")
+# Reading (if already calculated)
+matriz <- read.csv("matriz.csv", header = TRUE, sep = ",")
+
+data.table::setnames(matriz, "travel_time_p50", "travel_time") # If already calculated, not needed
 oportunidades_cumulativas <- cumulative_cutoff(
   travel_matrix = matriz,
-  land_use_data = df_final,
-  opportunity = "contagem_unica",
+  land_use_data = mdf,
+  opportunity = "P001",
   travel_cost = "travel_time",
   cutoff = 120,
-  active = TRUE
+  active = FALSE
 )
 head(oportunidades_cumulativas)
 
-#dataset_final = left_join(sp, oportunidades_cumulativas, by=c("id"))
+# Adding polygon to data
 oportunidades_cumulativas$geom <- cell_to_polygon(input = oportunidades_cumulativas$id, simple = TRUE)
-#dataset_final <- na.omit(dataset_final)
-#grade_gua <- aopdata::read_grid(city = "Guarulhos")
-#grade_sp <- aopdata::read_grid(city = "São Paulo")
-#grade <-bind_rows(grade_sp, grade_gua)
 base_sf <- st_as_sf(oportunidades_cumulativas, crs = 4674) 
 base_h3_sf <- st_transform(base_sf, crs = 4674)
 
-
+# Data for graph
 muns <- read_municipality(code_muni = "all")
 rmsp <- muns[muns$code_muni%in%c(3503901,3505708,3506607,
                                  3509007,3509205,3510609,3513009,
@@ -135,24 +155,37 @@ rmsp <- muns[muns$code_muni%in%c(3503901,3505708,3506607,
                                  3545001,3546801,3547304,3547809,
                                  3548708,3548807,3549953,3550308,
                                  3552502,3552809,3556453),]
-#rmsp$resolution <- 9
-#rmsp$id <- polygonToCells(rmsp$geom, resolution = rmsp$resolution)
-#colnames(grade)[colnames(grade) == "id_hex"] <- "id"
-#dataset_final = left_join(grade, oportunidades_cumulativas, by=c("id"))
+sao <- muns[muns$code_muni%in%c(3550308),]
 
-h3_dentro_multipolygon <- st_intersection(base_h3_sf, rmsp)
+# Merging accessibility data with Sao Paulo limits
+h3_dentro_multipolygon <- st_intersection(base_h3_sf, sao)
 
+# Replacing NA for 0, so it shows also on graph
+h3_dentro_multipolygon <- h3_dentro_multipolygon %>%
+  mutate(P001 = ifelse(is.na(P001), 0, P001))
+
+
+# Visualization
 ggplot() +
-  geom_sf(data = h3_dentro_multipolygon, aes(fill = contagem_unica), color = NA) +  # Remover as bordas dos hexágonos
-  geom_sf(data = rmsp, fill = NA, color = "white") +  # Multipolygon em cinza
+  geom_sf(data = h3_dentro_multipolygon, aes(fill = P001), color = NA) +  # Remove as bordas dos hexágonos
+  geom_sf(data = rmsp, fill = NA, color = "white") +  # Multipolygon em branco
   scale_fill_viridis_c(option = "inferno") +
-  labs(fill = "Quantas Oportunidade Acessíveis em até 120min") +
-  theme_minimal()
+  labs(
+    fill = "Estimativa",
+    title = "Quantas Pessoas Acessam em até 120min este hex?"  # Define o título do gráfico
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),  # Remove o quadriculado principal
+    panel.grid.minor = element_blank(),  # Remove o quadriculado menor
+    axis.text = element_blank(),         # Remove os valores dos eixos
+    axis.title = element_blank(),        # Remove os títulos dos eixos
+    axis.ticks = element_blank(),        # Remove as marcações dos eixos
+    plot.title = element_text(hjust = 0.5)  # Centraliza o título
+  )
 
+# Removing geom column for exporting
+df_sem_geom <- st_drop_geometry(oportunidades_cumulativas)
 
-#ggplot(dataset_final) +
-  #geom_sf(aes(geometry = geom)) +
-#  geom_sf(aes(geometry = geom, fill = contagem_unica),size=0) +
-#  scale_fill_viridis_c(option = "inferno") +
-#  labs(fill = "Quão acessível é cada hexágono") +
-#  theme_minimal()
+# Exporting Results
+write.csv(df_sem_geom, "Dados/accessibility.csv", row.names = FALSE)
