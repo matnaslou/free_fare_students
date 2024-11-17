0aa62ca3773fa4f3e937ccbeb4cc0f66ba64e81d
matnaslou <matheusnaslou@gmail.com>
2024-11-17 20:36:33 -0300

    - Reorder code for better understanding - Correlation Analysis for National or Sao Paulo level - Arguments for easier personalization

--- Scripts/7a - Análise Dados Censo Escolar 2014 (Escolha de Controles).R
+++ Scripts/7a - Análise Dados Censo Escolar 2014 (Escolha de Controles).R
@@ -3,12 +3,20 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 
+###################################################
+#################   LEITURA   #####################
+###################################################
 # Reading Scholar Census Data
 censo <- fread("Dados/Dados Censo/microdados_ed_basica_2014/dados/microdados_ed_basica_2014.csv")
 
 censo$tipo_escola <- factor(censo$TP_DEPENDENCIA, 
                             levels = c(1, 2, 3, 4), 
                             labels = c("Federal", "Estadual", "Municipal", "Privada"))
+
+
+###################################################
+#################   VARIÁVEIS   ###################
+###################################################
 
 # Criar a variável binária 'escola_privada'
 censo$escola_privada <- ifelse(censo$TP_DEPENDENCIA == 4, 1, 0)
@@ -36,34 +44,21 @@ censo$esc_mun_fac <- factor(censo$escola_municipal,
                             levels = c(0,1),
                             labels = c("Outra","Municipal"))
 
+censo$saopaulo <- ifelse(censo$CO_MUNICIPIO == 3550308, 1, 0) # 6995 escolas de São Paulo
 
-# Gerar o gráfico de densidade para cada tipo de escola (Pública ou Privada)
-var <- "TP_ATIVIDADE_COMPLEMENTAR"
-ggplot(censo, aes(x = get(var), color = esc_priv_fac)) +
-  geom_density(fill= NA, size = 1) + # alpha ajusta a transparência para sobreposição
-  labs(title = paste0("Densidade de ", var, " por Tipo de Escola"),
-       x = paste0(var),
-       y = "Densidade") +
-  scale_color_manual(values = c("Pública" = "blue", "Privada" = "red")) +
-  theme_minimal()
-
-# Gerar o gráfico de densidade para cada tipo de escola (Repartido por Tipo)
-var <- "IN_BANDA_LARGA"
-ggplot(censo, aes(x = get(var), color = tipo_escola)) +
-  geom_density(fill= NA, size = 1) + # alpha ajusta a transparência para sobreposição
-  labs(title = paste0("Densidade de ", var, " por Tipo de Escola"),
-       x = paste0(var),
-       y = "Densidade") +
-  scale_color_manual(values = c("Federal" = "blue", "Estadual" = "green", 
-                                "Municipal" = "orange", "Privada" = "red")) +
-  theme_minimal()
-
-
+# Base filtrando apenas São Paulo
+censosp <- censo[censo$saopaulo == 1,]
+censospem <- censosp[censosp$IN_MED == 1,]
+############################################
+#########   Matriz de Correlação   #########
+############################################
+# Escolha da Base (Nacional ou São Paulo)
+base <- "censospem"
+# Escolha do nível de correlação mínima
+corr <- 0.3
 # Selecionar apenas as variáveis numéricas para a matriz de correlação
-variaveis_numericas <- censo %>% select(where(is.numeric))
+variaveis_numericas <- get(base) %>% select(where(is.numeric))
 
-
-########### Matriz de Correlação ##############
 var2 <- "escola_privada"
 
 # Calcular a matriz de correlação
@@ -81,7 +76,7 @@ cor_tipo_escola_df$Variavel <- row.names(cor_tipo_escola_df)
 
 # Filtrar as correlações para valores >= 0.3 ou <= -0.3
 cor_tipo_escola_df_filtrado <- cor_tipo_escola_df %>%
-  filter(get(var2) <= -0.15 | get(var2) >= 0.15)
+  filter(get(var2) <= -corr | get(var2) >= corr)
 
 # Cria o gráfico de barras colorido para visualização das correlações
 ggplot(cor_tipo_escola_df_filtrado, aes(x = reorder(Variavel, get(var2)), y = get(var2))) +
@@ -92,3 +87,36 @@ ggplot(cor_tipo_escola_df_filtrado, aes(x = reorder(Variavel, get(var2)), y = ge
        y = "Correlação") +
   theme_minimal() +
   coord_flip() # Gira o gráfico para facilitar a leitura das labels
+
+###############################################
+########   VISUALIZAÇÃO DE VARIÁVEIS   ########
+###############################################
+# Escolha a Variável
+var <- "QT_TUR_MED"
+# Gerar o gráfico de densidade para cada tipo de escola (Pública ou Privada)
+ggplot(get(base), aes(x = get(var), color = esc_priv_fac)) +
+  geom_density(fill= NA, size = 1) + # alpha ajusta a transparência para sobreposição
+  labs(title = paste0("Densidade de ", var, " por Tipo de Escola"),
+       x = paste0(var),
+       y = "Densidade") +
+  scale_color_manual(values = c("Pública" = "blue", "Privada" = "red")) +
+  theme_minimal()
+
+# Gerar o gráfico de densidade para cada tipo de escola (Repartido por Tipo)
+ggplot(get(base), aes(x = get(var), color = tipo_escola)) +
+  geom_density(fill= NA, size = 1) + # alpha ajusta a transparência para sobreposição
+  labs(title = paste0("Densidade de ", var, " por Tipo de Escola"),
+       x = paste0(var),
+       y = "Densidade") +
+  scale_color_manual(values = c("Federal" = "blue", "Estadual" = "green", 
+                                "Municipal" = "orange", "Privada" = "red")) +
+  theme_minimal()
+
+
+############################################
+#########   ESCOLHA DE CONTROLES   #########
+############################################
+# Construção de Variáveis Possíveis Controles
+## Tem Laboratório?
+
+
